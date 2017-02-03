ebc5b865d07507cf371c1f911a3daeaa15a75e5b
Joabe Tavares Costa <jtcosta@scopus.com.br>
2017-02-03 16:12:29 -0200

    Adding FAB Toolbar to the project with adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FabToolbar } from './components/fab-toolbar/fab-toolbar';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FabToolbar
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FabToolbar
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,11 +8,52 @@ import { NavController, ActionSheetController, AlertController, FabContainer } f
 })
 export class HomePage {
 
+  public color: string = 'primary';
+  public position: string = 'right';
+  public icon: string = 'cart';
+  public enableBackdropDismiss: boolean = true;
+  public buttonColor: string = 'light';
+
   constructor(
     public navCtrl: NavController, 
     public actionSheetCtrl: ActionSheetController, 
-    public alertCtrl: AlertController,
+    public alertCtrl: AlertController
     ) { }
+
+    public buttons =  [
+      {
+        icon: 'add',
+        title: 'Sugar',
+        color: this.buttonColor,
+        handler: ()=> {
+          //this.presentToast('Dont close on click');
+        }
+      },
+      {
+        icon: 'checkmark',
+        title: 'Sugar',
+        color: this.buttonColor,
+        handler: ()=> {
+          //this.presentToast('Dont close on click');
+        }
+      },
+      {
+        icon: 'add', 
+        title: 'Coffee',
+        color: this.buttonColor,
+        handler: ()=> {
+          //this.presentToast('Close on click');
+        }
+      },
+      {
+        icon: 'checkmark',
+        title: 'Coffe',
+        color: this.buttonColor,
+        handler: ()=> {
+          //this.presentToast('Dont close on click');
+        }
+      }
+    ];
 
 openMenu(fab: FabContainer) {
     let actionSheet = this.actionSheetCtrl.create({
@@ -31,14 +72,6 @@ openMenu(fab: FabContainer) {
           text: 'Send a comment',
           icon: 'quote',
           handler: data => this.showPrompt()
-        },
-        {
-          text: 'Need to buy sugar!',
-          icon: 'cart'
-        },
-        {
-          text: 'Need to buy coffee polder!',
-          icon: 'cart'
         },
         {
           text: 'Cancel',
